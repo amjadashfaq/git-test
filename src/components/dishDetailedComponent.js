aa69730a5ef2d863758c0787008b5748e9fb7c3b
Amjad <muhammadamjad3583@gmail.com>
2020-08-19 12:34:26 +0500

    changes added

--- src/components/dishDetailedComponent.js
+++ src/components/dishDetailedComponent.js
@@ -3,34 +3,34 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
     CardTitle } from 'reactstrap';
 
     class DishDetail extends Component{
-        //constructor(props) {
-        //    super(props);
+        constructor(props) {
+            super(props);
 
-        //    this.state = {
-        //        selectedDish: null
-        //    };
-        //}
-        //onDishSelect(dish) {
-        //    this.setState({ selectedDish: dish});
-        //}
-        //renderDish(dish) {
-        //    if (dish != null)
-        //        return(
-        //            <Card>
-        //                <CardImg top src={dish.image} alt={dish.name} />
-        //                <CardBody>
-        //                  <CardTitle>{dish.name}</CardTitle>
-        //                  <CardText>{dish.description}</CardText>
-        //                </CardBody>
-        //            </Card>
-        //        );
-        //                else
-        //        return(
-        //            <div></div>
-        //        );
-        //                }
+            this.state = {
+                selectedDish: null
+            };
+        }
+        onDishSelect(dish) {
+            this.setState({ selectedDish: dish});
+        }
+        renderDish(dish) {
+            if (dish != null)
+                return(
+                    <Card>
+                        <CardImg top src={dish.image} alt={dish.name} />
+                        <CardBody>
+                          <CardTitle>{dish.name}</CardTitle>
+                          <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                );
+                        else
+                return(
+                    <div></div>
+                );
+                        }
 
-        const DishComponent = () => {
+        render() {
             return(
                 <div className="row">
                             <div  className="col-12 col-md-5 m-1">
